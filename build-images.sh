db49e12a3cd6aa60f42fd305b9c4b8b9c65257f1
Linh B Ngo <linh.b.ngo@gmail.com>
2020-04-11 14:05:57 -0400

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-docker image rm spark-worker | true
-docker image rm spark-master | true
-docker image rm spark-base | true
+docker image rm head | true
+docker image rm ldap | true
 
-dos2unix docker/spark-worker/start-worker.sh
-dos2unix docker/spark-master/start-master.sh
+#dos2unix docker/spark-worker/start-worker.sh
+#dos2unix docker/spark-master/start-master.sh
 
-docker build -t spark-base:latest ./docker/base
-docker build -t spark-master:latest ./docker/spark-master
-docker build -t spark-worker:latest ./docker/spark-worker
+docker build -t head:latest ./docker/head
+docker build -t ldap:latest ./docker/ldap
 
 echo "Launch the Dockerized Spark cluster with a specific number of workers"
 echo "docker-compose up -d --scale spark-worker=  "
