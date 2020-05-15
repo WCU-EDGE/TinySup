9e803ba163412b451b6a1e3da9cc187950d54636
Linh Ngo <lngo@wcupa.edu>
2020-05-15 14:18:42 -0400

    changes to test volume

--- docker/head/start_head.sh
+++ docker/head/start_head.sh
@@ -1,10 +1,10 @@
 #!/bin/#!/usr/bin/env bash
 
 # setup login-scheduler to use NFS from ldap-nfs
-bash /installation/nfs/NFS_client_config.sh
+#bash /installation/nfs/NFS_client_config.sh
 
 # setup ldap client Configuration
-cp /installation/ldap/ldap.conf.client /etc/openldap/ldap.conf
+#cp /installation/ldap/ldap.conf.client /etc/openldap/ldap.conf
 
 echo "Docker: login-scheduler is sleeping"
 sleep infinity
--- docker/ldap/start_ldap.sh
+++ docker/ldap/start_ldap.sh
@@ -1,14 +1,14 @@
 #!/bin/#!/usr/bin/env bash
 
 # configure NFS server on ldap-nfs
-bash /installation/nfs/NFS_server_config.sh
+# bash /installation/nfs/NFS_server_config.sh
 echo "Docker: finishes NFS startup ..."
 
 # configure LDAP server on ldap-nfs
 cp  installation/ldap/slapd.conf /etc/openldap/
 cp  installation/ldap/organization.ldif /etc/openldap/
 cp  installation/ldap/users.ldif /etc/openldap/
-bash /installation/ldap/launch_ldap.sh
+#bash /installation/ldap/launch_ldap.sh
 echo "Docker: finishes LDAP startup ..."
 
 echo "Docker: ldap-nfs is sleeping"
